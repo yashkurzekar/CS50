ba70842c93f2c72ee9e6ef0c45af87b32ab8ff5e
Yash Kurzekar <53827843+yashkurzekar@users.noreply.github.com>
2025-10-03 16:42:52 +0530

    Update credit.py
    Working Code all test cases getting passed

--- Week 6/Problem Set 6/Sentimental Credit More/credit.py
+++ Week 6/Problem Set 6/Sentimental Credit More/credit.py
@@ -1,91 +1,37 @@
 import sys
-
 from cs50 import get_int
 
-# Prompt the user for the credit card number (assume that the user enters only a number)
+# Prompt user
 number = get_int("Number: ")
+num_str = str(number)
 
-# Store the first 1 and 2 digits of the credit card number entered
-beginning2 = int(str(number)[:2])
-beginning1 = int(str(number)[:1])
+beginning2 = int(num_str[:2])
+beginning1 = int(num_str[:1])
+length = len(num_str)
 
-# Check if the credit card is valid
-if not (
-    beginning2 == 34
-    or beginning2 == 37
-    or beginning1 == 4
-    or beginning1 == 5
-    or beginning1 == 6
-):
-    print("INVALID")
-    sys.exit()
-
-# Vars
-product = 0
-everyotherdig = []
-others = []
-
-# Add 2 times the digit at every alternate index starting from the second last
-index = len(str(number)) - 2
-for j in range(index, -2, -1):
-    if (index - j) % 2 == 0:
-        dig = int(str(number)[j])
-        everyotherdig.append(dig * 2)
-    # If the number wasn't multiplied by 2, add to another list (this will be useful later)
-    else:
-        others.append(int(str(number)[j]))
-
-# Split numbers that are 2 digits into 2 numbers
-for i, item in enumerate(everyotherdig):
-    if len(str(item)) == 2:
-        everyotherdig.append(int(str(item)[0]))
-        everyotherdig.append(int(str(item)[1]))
-        everyotherdig.pop(i)
-
-# Add all the numbers in the list together
+# Luhn checksum
 sumdigits = 0
-for i, item in enumerate(everyotherdig):
-    sumdigits += item
+reverse_digits = num_str[::-1]
 
-# Add that sum to the digits that weren't multiplied by 2
-othersum = 0
-for i, item in enumerate(others):
-    othersum += item
+for i, d in enumerate(reverse_digits):
+    n = int(d)
+    if i % 2 == 1:   # every other digit
+        n *= 2
+        if n > 9:
+            n = n // 10 + n % 10
+    sumdigits += n
 
-finalsum = sumdigits + othersum
+valid = (sumdigits % 10 == 0)
 
-# Check if the last digit in the sum is 0
-valid = True
-lastdigit = int(str(finalsum)[-1])
-
-if lastdigit == 0:
-    # If yes, then the credit card number passes the checksum
-    valid = True
-elif lastdigit != 0:
-    valid = True
-
-# If the card is valid, then find which company made it
-if valid is True and len(str(number)) >= 13:
-    # Check if it is an American Express card
-    if beginning2 == 34 or beginning2 == 37:
+# Identify card
+if valid:
+    if length == 15 and (beginning2 == 34 or beginning2 == 37):
         print("AMEX")
-        sys.exit()
-    # Check if it is a MasterCard
-    elif (
-        beginning2 == 51
-        or beginning2 == 52
-        or beginning2 == 53
-        or beginning2 == 54
-        or beginning2 == 55
-    ):
+    elif length == 16 and 51 <= beginning2 <= 55:
         print("MASTERCARD")
-        sys.exit()
-    # Check if it is a Visa card
-    elif beginning1 == 4:
+    elif (length == 13 or length == 16) and beginning1 == 4:
         print("VISA")
-        sys.exit()
     else:
         print("INVALID")
-        sys.exit()
-
-print("INVALID")
+else:
+    print("INVALID")
